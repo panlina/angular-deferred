050df048dae900f77ab2036f0249477d64d0d7ac
Jack Lu <jacklu@jacklu.me>
2017-02-18 23:56:33 +0800

    +deferredReplaceConfig

--- deferredReplace.js
+++ deferredReplace.js
@@ -1,14 +1,14 @@
 ﻿angular.module('deferred')
-	.directive('deferredReplace', function () {
+	.directive('deferredReplace', function (deferredReplaceConfig) {
 		return {
 			restrict: 'E',
 			require: "^deferred",
 			compile: function (element, attr) {
 				var parent = element.parent();
 				var replacee = parent.parent();
-				replacee.after("<div ng-hide=$deferred." + parent.attr('name') + ">ready</div>");
-				replacee.after("<div ng-show=$deferred." + parent.attr('name') + ".$$state.status==0>loading..</div>");
-				replacee.after("<div ng-show=$deferred." + parent.attr('name') + ".$$state.status==2>error</div>");
+				replacee.after(angular.element(deferredReplaceConfig.ready).attr('ng-hide', "$deferred." + parent.attr('name')));
+				replacee.after(angular.element(deferredReplaceConfig.loading).attr('ng-show', "$deferred." + parent.attr('name') + ".$$state.status==0"));
+				replacee.after(angular.element(deferredReplaceConfig.error).attr('ng-show', "$deferred." + parent.attr('name') + ".$$state.status==2"));
 				return function (scope, element, attr, deferred) {
 					replacee.addClass('ng-hide');
 					scope.$watch(function () { return deferred.promise; }, function (value) {
@@ -21,5 +21,13 @@
 					})
 				};
 			}
+		};
+	})
+	.provider('deferredReplaceConfig', function () {
+		return {
+			ready: "<div>ready</div>",
+			loading: "<div>loading..</div>",
+			error: "<div>error</div>",
+			$get: function () { return this; }
 		};
 	})
